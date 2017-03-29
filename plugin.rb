02186df61909bb4154f325d00f33c5a1a92a9034
Rimian Perkins <hello@rimian.com.au>
2017-03-29 14:29:32 +1100

    bumpus

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-donations
 # about: Integrating Discourse with Stripe for donations
-# version: 1.7.0.5
+# version: 1.7.0.6
 # url: https://github.com/choiceaustralia/discourse-donations
 # authors: Rimian Perkins
 
